dbe840153a295067df3dd392187d90f788a104d9
Basith Ahmed <143781280+basith-ahmed@users.noreply.github.com>
2024-10-06 23:09:05 +0530

    never gonna give you up

--- console.js
+++ console.js
@@ -1,11 +1,11 @@
+// switch tabs
+Object.defineProperty(document, 'visibilityState', {value: 'visible', writable: false});
+Object.defineProperty(document, 'hidden', {value: false, writable: false});
+
 // copy and paste
 document.addEventListener('copy', (event) => { event.stopPropagation(); }, true);
 document.addEventListener('cut', (event) => { event.stopPropagation(); }, true);
 document.addEventListener('paste', (event) => { event.stopPropagation(); }, true);
-
-// switch tabs
-Object.defineProperty(document, 'visibilityState', {value: 'visible', writable: false});
-Object.defineProperty(document, 'hidden', {value: false, writable: false});
 
 // full screen
 Object.defineProperty(document, 'fullscreenElement', { get: () => null });
