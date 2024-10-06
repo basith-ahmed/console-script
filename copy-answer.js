78fa87ea46ef0c6d43930f624894b809b9844942
Basith Ahmed <143781280+basith-ahmed@users.noreply.github.com>
2024-10-06 22:46:12 +0530

    never gonna let you down

--- copy-answer.js
+++ copy-answer.js
@@ -27,3 +27,19 @@ function copyDivText(divSelector) {
 }
 
 copyDivText('.ace_text-layer');
+
+// 3: Updated pre defined div class
+function copyDivText(divSelector) {
+    const divs = document.querySelectorAll(divSelector);
+    const div = divs[1];
+    const text = div ? div.innerText : '';
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    console.log('Text copied to clipboard:', text);
+}
+
+copyDivText('.ace_text-layer');
